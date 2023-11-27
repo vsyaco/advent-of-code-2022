785f32fce503fe3db2333e60e1e82c91f1b22ae7
Dmitry Lakhno <dmitry@vsyaco.com>
2023-11-27 22:40:31 +0300

    Day 1, Part 2: JS

--- day1/js/main.js
+++ day1/js/main.js
@@ -1,13 +1,31 @@
 import { open } from 'node:fs/promises';
 
 var current = 0,
-    max = 0
+    max = [0]
 
 function onUserElfChanged() {
-    if (current >= max) {
-        max = current;
+
+    const cur = current
+    current = 0
+
+    if (max.length < 3) {
+        max.push(cur)
+        return
     }
-    current = 0;
+
+    let min = max[0]
+    let minIndex = 0;
+    for (let i = 0; i < max.length; ++i) {
+        if (max[i] < min) {
+            min = max[i]
+            minIndex = i
+        }
+    }
+
+    if (min < cur) {
+        max[minIndex] = cur 
+    }
+
 }
 
 const file = await open('../data.txt');
@@ -19,4 +37,4 @@ for await (const line of file.readLines()) {
     current += parseInt(line)
 }
 
-console.log(max)
+console.log(max.reduce((a, b) => a + b, 0))
